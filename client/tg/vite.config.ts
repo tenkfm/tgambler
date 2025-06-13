2fcd3ccfce68538d76540f7fed6bb88191f80635
Anton Rogachevskyi <anton.rogachevskyi@gmail.com>
2025-06-13 14:40:41 +0200

    change name

--- client/tg/vite.config.ts
+++ client/tg/vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gifter/',
+  base: '/tgambler/',
   css: {
     preprocessorOptions: {
       scss: {
