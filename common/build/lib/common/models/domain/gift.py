29bec6aa420be7018606aa1615bdaee87d443627
Tony <anton.rogachevskyi@gmail.com>
2025-06-23 16:59:13 +0200

    common package

---
+++ common/build/lib/common/models/domain/gift.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from common.services.firebase.firebase_object import FirebaseObject
+
+class GiftType(str, Enum):
+    GIFT = "GIFT"
+    BALANCE = "BALANCE"
+
+class Gift(FirebaseObject):
+    case_id: str
+    name: str
+    image_url: str
+    background: str
+    volume: int
+    prob: int
+    is_active: bool
+    type: GiftType
+
+    def probf(self) -> float:
+        """
+        Calculate the probability as a percentage.
+        :return: Probability as a float between 0 and 1.
+        """
+        return self.prob / 100.0 if self.prob else 0.0
+    
+    def volumef(self) -> float:
+        """
+        Calculate the probability as a percentage.
+        :return: Probability as a float between 0 and 1.
+        """
+        return self.volume / 100.0 if self.volume else 0.0
+    
+    @staticmethod
+    def collection_name():
+        return "gifts"  # Firestore collection for User instances
---
+++ common/build/lib/common/models/domain/wallet.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from typing import Optional, Dict, Any
+from datetime import datetime
+from pydantic import BaseModel
+from common.services.firebase.firebase_object import FirebaseObject
+
+class Currency(str, Enum):
+    TON = "TON"
+    COIN = "COIN"
+    XTR = "XTR"
+
+class TopUpStatus(str, Enum):
+    PENDING = "PENDING"
+    PROCESSING = "PROCESSING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+
+class Wallet(FirebaseObject):
+    user_id: str
+    balance: int = 0
+    currency: Currency
+    last_updated: Optional[datetime] = None  # Timestamp of the last update
+
+    @staticmethod
+    def collection_name():
+        return "wallets"
+
+class Transaction(FirebaseObject):
+    from_wallet_id: str  # ID of the wallet initiating the transaction
+    to_wallet_id: str  # ID of the wallet receiving the transaction
+    amount: int # Amount of the transaction in cents
+    currency: Currency  # Currency of the transaction
+    timestamp: datetime = datetime.now()  # Timestamp of the transaction
+    description: str  # Description of the transaction
+
+    @staticmethod
+    def collection_name():
+        return "transactions"
+
+class TopUpRequest(FirebaseObject):
+    user_id: str
+    amount: int # In cents
+    provider: str
+    currency: Currency
+    external_id: str
+    status: TopUpStatus
+    payload: Optional[str] = None
+    info: Optional[Dict[str, Any]] = None
+    created_at: datetime = datetime.now()
+
+    @staticmethod
+    def collection_name():
+        return "topup_requests"
