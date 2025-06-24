96de36fd758f44f7fac8e48e67f5645d23348c10
Tony <anton.rogachevskyi@gmail.com>
2025-06-24 16:23:03 +0200

    common version

--- common/setup.py
+++ common/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="common",            # именно так, чтобы при импорте было `import common`
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),  # найдёт папку common/common
 )
